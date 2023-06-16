5bb99b4584d41f51a252a9dbb2eec1ae0a1fe443
Irina <i.sus2011@yandex.ru>
2023-06-16 12:27:50 +0300

    описать  функции

--- loading-indicator/uploadingFiles/uploadingFiles.js
+++ loading-indicator/uploadingFiles/uploadingFiles.js
@@ -3,9 +3,8 @@ const BYTES_IN_MB = 1024 * 1024;
 const form = document.getElementById("form");
 const fileInput = document.getElementById("fileInput");
 const fileList = document.getElementById("fileList");
-const files = fileInput.files; // Обновляем список файлов
 
-// Очищаем список файлов перед обновлением
+//функция очистки файлов перед обновлением
 function clearList() {
   while (fileList.firstChild) {
     fileList.firstChild.remove();
@@ -13,20 +12,29 @@ function clearList() {
 }
 
 fileInput.addEventListener("change", function () {
-  clearList();
+  clearList(); //очищаем список файлов перед обновлением
   
-  const files = fileInput.files; // Обновляем список файлов
+  
+  //Получаем доступ к информации о выбранных файлах При помощи свойства.files
+  //Обновляем переменную files, весь список файлов
+  const files = fileInput.files; 
+  //Цикл for выполняется последовательно для каждого файла   
   for (let i = 0; i < files.length; i++) {
+  //Извлекаем текущий файлиз списка и сохраняем его в const file
     const file = files[i];
 
     //проверяем, если файл больше 5MB выводим предупреждение
+    //Но можно это не использовать, эта проверка по желанию
     if (file.size > 5 * BYTES_IN_MB) {
      alert ("Слишком большой файл");
       fileInput.value = null;
       continue;
     }
+    //Появляется строчка
     const uploadedFile = document.createElement("printFile");
+    //в которой появится текст, соответствующий имени файла
     uploadedFile.textContent = file.name;
+    //Если файлов несколько, то это будет список
     fileList.appendChild(uploadedFile);
   }
 });
@@ -34,13 +42,15 @@ fileInput.addEventListener("change", function () {
 form.addEventListener("submit", function (event) {
   //Отменяем стандартную отправку формы
   event.preventDefault();
+  //Опять обновляется const files
   const files = fileInput.files; 
-
+ //Цикл for выполняется последовательно для каждого файла   
   for (let i = 0; i < files.length; i++) {
    //выбирается текущий файл 
     const file = files[i];
     //создается новый объект
     const formData = new FormData();
+    //Метод append принимает два параметра: имя поля и значение
     formData.append("file", file);
 
     //отправляется асинхронный запрос
@@ -50,10 +60,12 @@ form.addEventListener("submit", function (event) {
     })
     //ответ с сервера преобразуется в формат JSON
       .then((response) => response.json())
+      //сервер возвращает имя файла
       .then((data) => {
-        // Файл успешно загружен
+        //Имя этого файла должно быть написано здесь
         const uploadedFile = document.createElement("printFile");
         uploadedFile.textContent = data.filename;
+         //Если файлов несколько, то это будет список
         fileList.appendChild(uploadedFile);
       })
       .catch((error) => {
